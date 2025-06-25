e4ef1b7114b9f1820d9288433866288aa935a380
Harley Kradovill <harleykradovill@gmail.com>
2025-06-24 23:39:48 -0600

    update default seconds

--- Jellyfin.Plugin.StopSessions/StopSessionsPlugin.cs
+++ Jellyfin.Plugin.StopSessions/StopSessionsPlugin.cs
@@ -13,7 +13,7 @@ namespace Jellyfin.Plugin.StopSessions;
 /// </summary>
 public class StopSessionsPlugin : BasePlugin<PluginConfiguration>, IHasWebPages
 {
-    private readonly Guid _id = new("EC9E2A74-1311-4A14-B302-158E3D95FD1D");
+    private readonly Guid _id = new("28a6125c-cb57-4e8b-b031-421ac55cfa91");
 
     /// <summary>
     /// Initializes a new instance of the <see cref="StopSessionsPlugin"/> class.
