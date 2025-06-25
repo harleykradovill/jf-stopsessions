06839ff5e1809708cc2c9b7f4d19d45c24354877
Harley Kradovill <harleykradovill@gmail.com>
2025-06-24 19:29:48 -0600

    first commit

---
+++ Jellyfin.Plugin.SessionCleaner/SessionCleanerTask.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Jellyfin.Data.Queries;
+using MediaBrowser.Controller.Devices;
+using MediaBrowser.Controller.Session;
+using MediaBrowser.Model.Globalization;
+using MediaBrowser.Model.Tasks;
+
+namespace Jellyfin.Plugin.SessionCleaner;
+
+/// <summary>
+/// Device cleaner task.
+/// </summary>
+public class SessionCleanerTask : IScheduledTask, IConfigurableScheduledTask
+{
+    private readonly IDeviceManager _deviceManager;
+    private readonly ISessionManager _sessionManager;
+    private readonly ILocalizationManager _localizationManager;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SessionCleanerTask"/> class.
+    /// </summary>
+    /// <param name="sessionManager">Instance of the <see cref="ISessionManager"/> interface.</param>
+    /// <param name="localizationManager">Instance of the <see cref="ILocalizationManager"/> interface.</param>
+    /// <param name="deviceManager">Instance of the <see cref="IDeviceManager"/> interface.</param>
+    public SessionCleanerTask(
+        ISessionManager sessionManager,
+        ILocalizationManager localizationManager,
+        IDeviceManager deviceManager)
+    {
+        _sessionManager = sessionManager;
+        _localizationManager = localizationManager;
+        _deviceManager = deviceManager;
+    }
+
+    /// <inheritdoc />
+    public bool IsHidden => false;
+
+    /// <inheritdoc />
+    public bool IsEnabled => true;
+
+    /// <inheritdoc />
+    public bool IsLogged => true;
+
+    /// <inheritdoc />
+    public string Name => "Clean Old Sessions";
+
+    /// <inheritdoc />
+    public string Key => "CleanOldSessions";
+
+    /// <inheritdoc />
+    public string Description => "Removes sessions older then the configured age.";
+
+    /// <inheritdoc />
+    public string Category => _localizationManager.GetLocalizedString("TasksMaintenanceCategory");
+
+    /// <inheritdoc />
+    public async Task ExecuteAsync(IProgress<double> progress, CancellationToken cancellationToken)
+    {
+        ArgumentNullException.ThrowIfNull(SessionCleanerPlugin.Instance?.Configuration);
+        var expireDays = SessionCleanerPlugin.Instance.Configuration.Days;
+        var expireDate = DateTime.UtcNow.AddDays(expireDays * -1);
+        var deviceResult = _deviceManager.GetDevices(new DeviceQuery());
+        var devices = deviceResult?.Items;
+
+        if (devices is null)
+        {
+            return;
+        }
+
+        foreach (var device in devices)
+        {
+            if (device.DateLastActivity < expireDate)
+            {
+                await _sessionManager.Logout(device).ConfigureAwait(false);
+            }
+        }
+    }
+
+    /// <inheritdoc />
+    public IEnumerable<TaskTriggerInfo> GetDefaultTriggers()
+    {
+        return Enumerable.Empty<TaskTriggerInfo>();
+    }
+}
---
+++ README.md
@@ -0,0 +1,34 @@
+<h1 align="center">Jellyfin Session Cleaner Plugin</h1>
+<h3 align="center">Part of the <a href="https://jellyfin.org">Jellyfin Project</a></h3>
+
+<p align="center">
+<img alt="Plugin Banner" src="https://raw.githubusercontent.com/jellyfin/jellyfin-ux/master/plugins/SVG/jellyfin-plugin-sessioncleaner.svg?sanitize=true"/>
+<br/>
+<br/>
+<a href="https://github.com/jellyfin/jellyfin-plugin-sessioncleaner/actions?query=workflow%3A%22Test+Build+Plugin%22">
+<img alt="GitHub Workflow Status" src="https://img.shields.io/github/workflow/status/jellyfin/jellyfin-plugin-sessioncleaner/Test%20Build%20Plugin.svg">
+</a>
+<a href="https://github.com/jellyfin/jellyfin-plugin-sessioncleaner">
+<img alt="GPLv3 License" src="https://img.shields.io/github/license/jellyfin/jellyfin-plugin-sessioncleaner.svg"/>
+</a>
+<a href="https://github.com/jellyfin/jellyfin-plugin-sessioncleaner/releases">
+<img alt="Current Release" src="https://img.shields.io/github/release/jellyfin/jellyfin-plugin-sessioncleaner.svg"/>
+</a>
+</p>
+
+## About
+
+This plugin allows automatic cleaning of leftover sessions.
+
+## Installation
+
+[See the official documentation for install instructions](https://jellyfin.org/docs/general/server/plugins/index.html#installing).
+
+## Contributing
+
+We welcome all contributions and pull requests! If you have a larger feature in mind please open an issue so we can discuss the implementation before you start.
+In general refer to our [contributing guidelines](https://github.com/jellyfin/.github/blob/master/CONTRIBUTING.md) for further information.
+
+## Licence
+
+This plugins code and packages are distributed under the GPLv3 License. See [LICENSE](./LICENSE) for more information.
